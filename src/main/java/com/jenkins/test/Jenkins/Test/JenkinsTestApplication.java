7d14f2838debe8127447f5008a05b172a38f6ec2
Nivrutti Patil <nivruttipatil89@gmail.com>
2023-03-25 18:03:18 +0530

    war deployment config added

--- src/main/java/com/jenkins/test/Jenkins/Test/JenkinsTestApplication.java
+++ src/main/java/com/jenkins/test/Jenkins/Test/JenkinsTestApplication.java
@@ -2,12 +2,13 @@ package com.jenkins.test.Jenkins.Test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class JenkinsTestApplication {
+public class JenkinsTestApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JenkinsTestApplication.class, args);
+	public static void main( String[] args ) {
+		SpringApplication.run( JenkinsTestApplication.class, args );
 	}
 
 }
