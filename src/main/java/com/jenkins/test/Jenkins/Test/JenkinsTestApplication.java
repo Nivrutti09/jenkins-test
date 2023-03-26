085f87af487afcc98930481e5f94089d9fa88663
Nivrutti Patil <nivruttipatil89@gmail.com>
2023-03-26 16:34:40 +0530

    war changes removed

--- src/main/java/com/jenkins/test/Jenkins/Test/JenkinsTestApplication.java
+++ src/main/java/com/jenkins/test/Jenkins/Test/JenkinsTestApplication.java
@@ -2,10 +2,9 @@ package com.jenkins.test.Jenkins.Test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class JenkinsTestApplication extends SpringBootServletInitializer {
+public class JenkinsTestApplication {
 
 	public static void main( String[] args ) {
 		SpringApplication.run( JenkinsTestApplication.class, args );
