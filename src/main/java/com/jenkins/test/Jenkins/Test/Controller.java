b0b86785ee5eef44fd0de6c130206e3d8d9d9920
Nivrutti Patil <nivruttipatil89@gmail.com>
2023-07-07 02:22:43 +0530

    github commit

--- src/main/java/com/jenkins/test/Jenkins/Test/Controller.java
+++ src/main/java/com/jenkins/test/Jenkins/Test/Controller.java
@@ -8,7 +8,7 @@ public class Controller {
 
 	@GetMapping( "/" )
 	public String hello( Model model ) {
-		model.addAttribute( "name", "Hello Jenkins -> Trigger from github commit" );
+		model.addAttribute( "name", "Hello Jenkins -> Trigger from github commit test" );
 		return "message";
 	}
 }
